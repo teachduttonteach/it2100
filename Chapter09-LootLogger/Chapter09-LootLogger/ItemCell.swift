63bf37f72631796c7b9e3f996785fa3f22a7fc27
John Dutton <teachduttonteach@gmail.com>
2021-03-10 22:11:37 -0500

    Chapters 9 and 10

---
+++ Chapter09-LootLogger/Chapter09-LootLogger/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  Chapter09-LootLogger
+//
+//  Created by John Dutton on 3/10/21.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
