2954682b64ebdb0fbeb497032759934f28621019
John Dutton <teachduttonteach@gmail.com>
2021-04-18 11:11:15 -0400

    Photorama-rama

---
+++ Chapter20-Photorama/Chapter20-Photorama/Photo.swift
@@ -0,0 +1,23 @@
+//
+//  Photo.swift
+//  Chapter20-Photorama
+//
+//  Created by John Dutton on 4/18/21.
+//
+
+import Foundation
+
+class Photo: Codable {
+    let title: String
+    let remoteURL: URL?
+    let photoID: String
+    let dateTaken: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case remoteURL = "url_z"
+        case photoID = "id"
+        case dateTaken = "datetaken"
+    }
+
+}
